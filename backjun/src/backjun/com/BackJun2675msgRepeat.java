9a4fe23441073a9d899fc275945ae26c62769682
jihoon <jihoon@DESKTOP-DH9A8MU>
2019-04-26 18:46:33 +0900

    테스트 케이스 실수 난거 수정

--- backjun/src/backjun/com/BackJun2675msgRepeat.java
+++ backjun/src/backjun/com/BackJun2675msgRepeat.java
@@ -6,18 +6,33 @@ public class BackJun2675msgRepeat {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int repeat = sc.nextInt();
+		int testCase = sc.nextInt();
 		sc.nextLine();
-		String msg = sc.nextLine();
+		String[] input = new String[testCase];
 		
-		String[] msgArr = msg.split("");
-		for(int i=0;i<msgArr.length;i++) 
+		for(int i=0;i<testCase;i++)
 		{
-			for(int j=0;j<repeat;j++) 
-			{
-				System.out.print(msgArr[i]);
-			}
+			input[i] = sc.nextLine();
 		}
+
 		
+		for(int i=0;i<testCase;i++)
+		{			
+			String[] data = input[i].split(" ");
+			int repeat = Integer.parseInt(data[0]);
+			
+			if(data.length>1) 
+			{
+				String[] msgArr = data[1].split("");
+				for(int k=0;k<msgArr.length;k++) 
+				{
+					for(int j=0;j<repeat;j++) 
+					{
+						System.out.print(msgArr[k]);
+					}
+				}
+			}
+			System.out.println();
+		}
 	}
 }
