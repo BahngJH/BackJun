6a024f829541871f9c7321e6b695decb4573d2ac
jihoon <jihoon@DESKTOP-DH9A8MU>
2019-04-29 19:24:14 +0900

    분수 찾기 구현

--- backjun/src/backjun/com/BackJun1193SearchBunSu.java
+++ backjun/src/backjun/com/BackJun1193SearchBunSu.java
@@ -10,12 +10,11 @@ public class BackJun1193SearchBunSu {
 		int colum =1;
 		int row = 1;
 		int result = 1;
+		boolean isSearch = false;
 		
-		while(true)
+		while(n>1)
 		{
-			if(n==1)
-				break;
-			
+			//행이 1일 때
 			if(colum==1) 
 			{
 				row++;
@@ -23,21 +22,26 @@ public class BackJun1193SearchBunSu {
 				
 				if(result==n)
 					break;
-				
+				//대각선 위에서 아래로 내려오는 로직
 				while(true)
 				{
 					row--;
 					colum++;
 					result++;
-					
-					if(result==n)
+					if(result==n) 
+					{
+						isSearch=true;
 						break;
+					}
 					if(row==1)
 						break;
-					
 				}
 			}
+			//원하는 분수를 찾았을 때
+			if(isSearch==true)
+				break;
 			
+			//열이 1일 때
 			if(row==1)
 			{
 				colum++;
@@ -45,18 +49,24 @@ public class BackJun1193SearchBunSu {
 				
 				if(result==n)
 					break;
-				
+				//대각선 아래서 위로 올라가는 로직
 				while(true)
 				{
 					colum--;
 					row++;
 					result++;
-					if(result==n)
+					if(result==n) 
+					{
+						isSearch=true;
 						break;
+					}
 					if(colum==1)
 						break;
 				}
 			}
+			//원하는 분수를 찾았을 때
+			if(isSearch==true)
+				break;
 		}
 		System.out.println(colum+"/"+row);
 	}
